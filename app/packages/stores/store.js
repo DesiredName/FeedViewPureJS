e19fd278128770f010118f75e75fe658214aa273
DesiredName <wc3-enter@yandex.ru>
2025-12-22 11:24:08 +0100

    'update'

--- app/packages/stores/store.js
+++ app/packages/stores/store.js
@@ -1,10 +1,22 @@
 const createStore = ({ state: _stt, mutations: _mts, actions: _acts }) => {
     const listeners = new Set();
+    let isBatching = false;
+
+    const notify = () => {
+        if (isBatching === true) return;
+
+        isBatching = true;
+
+        queueMicrotask(() => {
+            listeners.forEach((listener) => listener(state));
+            isBatching = false;
+        });
+    };
 
     const state = new Proxy(_stt, {
         set(target, key, value) {
             target[key] = value;
-            listeners.forEach((listener) => listener(target));
+            notify();
             return true;
         },
     });
